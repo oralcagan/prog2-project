1fd2a38d1462450002937d67484d404920c4e204
Çağan <oralcagan@gmail.com>
2024-04-24 16:33:08 +0200

    Added the IDEA project to the folder

--- cagan-code/src/Main.java
+++ cagan-code/src/Main.java
@@ -3,11 +3,12 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         int turns = 50;
+        HashMap<String,Integer> map = new HashMap<>();
         while(turns != 0) {
             turns--;
             Person ozberk = new Person(1.F,1.F,"Ozberk");
             Person flavio = new Person(0.01F,5.F,"Flavio");
-            Person cagan = new Person(0.5F,0.5F,"Cagan");
+            Person cagan = new Person(1000.F,0.5F,"Cagan");
             List<Person> people = Arrays.asList(ozberk,cagan,flavio);
             Random rand = new Random();
             while(people.size() > 1) {
@@ -28,11 +29,16 @@ public class Main {
                 }
                 people = newPeople;
             }
-            System.out.println("-----");
-            for (Person person : people) {
-                System.out.println(person.name);
+            if(people.isEmpty()) {
+                Integer oldNum = map.getOrDefault("No one", 0);
+                map.put("No one",oldNum + 1);
+                continue;
             }
+            Person lastPerson = people.getFirst();
+            Integer oldNum = map.getOrDefault(lastPerson.name, 0);
+            map.put(lastPerson.name,oldNum + 1);
         }
+        System.out.println(map.toString());
     }
 }
 

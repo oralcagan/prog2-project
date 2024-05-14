fb3214493ca57945f99fecf664ba7d39487dac90
flavi <flaviomartelli4@gmail.com>
2024-05-14 18:08:29 +0200

    adding timer

--- cagan-code/src/Main.java
+++ cagan-code/src/Main.java
@@ -2,9 +2,12 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
+        long startTime = System.nanoTime();
         City napoli = new City(5,100,0.6F);
         for (int i = 1; i <100; i ++){
             napoli.runTurn();
         }
+        long endTime = System.nanoTime();
+        long total = endTime - startTime;
     }
 }
